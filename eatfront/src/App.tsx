87bb4b77d589a63e085a13fe20b0d473f0725d0f
kwongwangjae <kgy30002@gmail.com>
2023-05-02 01:38:58 +0900

    feat: settings

---
+++ eatfront/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">안녕하세세용</div>
+  );
+}
+
+export default App;
